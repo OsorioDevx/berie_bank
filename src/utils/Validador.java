0b76c71e55b7d2bb19289e9d6fad7168c95caf4a
Matheus Henrique Gama Osório <143817914+OsorioDevx@users.noreply.github.com>
2026-07-31 10:43:13 -0400

    abc

--- src/utils/Validador.java
+++ src/utils/Validador.java
@@ -15,10 +15,33 @@ public class Validador {
         validação trim + isEmpty*/
     }
 
-
+    /*Só é possível chamar essas classes diretamente sem criar nenhum objeto porque elas são static, não guardam estado*/
     public static boolean cpfValido(String cpf) {
-        if (cpf == null ){
-
+        if (cpf == null || cpf.trim().isEmpty()){
+            return false;
         }
+
+        String limpo = cpf.trim();
+
+        if (limpo.length() != 11) {
+            return false;
+        }
+
+        /*aqui só vai faltar uma validação que vou implementar depois que verifica os dois últimos
+         * dígitos que são baseados nos 9 primeiros, nesse estado atual se alguém digitar 11111111111
+         * ele considera como um cpf válido*/
+        for (int i = 0; i < limpo.length(); i++) {
+            char caractere = limpo.charAt(i);
+            if (caractere < '0' || caractere > '9'){
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+    /*saque. depósito, investimento, tudo tem que ser >0*/
+    public static boolean valorPositivo(double valor) {
+        return valor > 0;
     }
 }
